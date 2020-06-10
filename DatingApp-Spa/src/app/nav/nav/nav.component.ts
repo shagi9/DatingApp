129610bfa9587370e6e17b75126203c2bc9b51c8
illya shagoferov <shagoferov@gmail.com>
2020-06-10 13:23:08 +0300

    implemented decoding token

--- DatingApp-Spa/src/app/nav/nav/nav.component.ts
+++ DatingApp-Spa/src/app/nav/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from 'src/app/services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
