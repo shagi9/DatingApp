f91fe0f28d1f290c7a394bcd237466ce1a17dd44
illya shagoferov <shagoferov@gmail.com>
2020-07-06 15:38:20 +0300

    added possibility to update info on spa

--- DatingApp-Spa/src/app/components/members/member-edit/member-edit.component.ts
+++ DatingApp-Spa/src/app/components/members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { User } from 'src/app/models/user';
 import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from 'src/app/services/alertify/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/services/user/user.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -17,10 +19,12 @@ export class MemberEditComponent implements OnInit {
       $event.returnValue = true;
     }
   }
+
   user: User;
 
   constructor(private route: ActivatedRoute, 
-    private alertify: AlertifyService) { }
+    private alertify: AlertifyService, private userService: UserService,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
@@ -29,8 +33,11 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertify.success('Profile updated successfully');
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated successfully');
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error(error);
+    })
   }
 }
